5dc16007b1ce143da8fa5525d04182749ebc0120
alliyahtaylor <serenatskino6@gmail.com>
2019-02-06 23:47:02 -0500

    Added a bunch of regular expressions
    Is this enough to justify a commit? Probably not. Am I going to commit anyway to prevent an "everything" commit? Absolutely. Can't actually test anything because I'm on a work machine so I'll do that later.
    
    _________________________________
    
    INT. PROMENADE
    
    SISKO walks briskly through the premenade as an AMBASSADOR WITH A BAD HAIRCUT AND POINTED EARS follows close behind, attempted to have a conversation.
    
    AMBASSADOR
    Commander, I need to know the status of Ambassador J'Tel, her Mission is -
    
    SISKO
    -Of utmost important, yes. You've informed me. Unfortunately, we can't send a vessel to rendezvous with her while the energy fluctuations persist. It's too dangerous.

--- dist/scripts/lexer.js
+++ dist/scripts/lexer.js
@@ -1,7 +1,7 @@
 /* lexer.ts  */
 var TSC;
 (function (TSC) {
-    var Lexer = (function () {
+    var Lexer = /** @class */ (function () {
         function Lexer() {
         }
         Lexer.lex = function () {
@@ -11,10 +11,66 @@ var TSC;
                 // Trim the leading and trailing spaces.
                 sourceCode = TSC.Utils.trim(sourceCode);
                 // TODO: remove all spaces in the middle; remove line breaks too.
+                /* Regular Expressions
+                * 	Contains the RegEx for our grammar */
+                //Types
+                //Character
+                var regChar = new RegExp(/[a-z] | /);
+                //ID
+                var regID = new RegExp(/[a-z]/);
+                //Digit
+                var regDigit = new RegExp(/[0-9]/);
+                //Symbols
+                //EOP
+                var regEOP = new RegExp(/\$/);
+                //Left Brace
+                var regLeftBrace = new RegExp(/{/);
+                //Right Brace
+                var regRightBrace = new RegExp(/}/);
+                //Left Parenthesis
+                var regLeftParen = new RegExp(/\(/);
+                //Right Parenthesis
+                var regRightParen = new RegExp(/\)/);
+                //Quote
+                var regQuote = new RegExp(/"/);
+                //Assign
+                var regAssign = new RegExp(/=/);
+                //Boolop Equal
+                var regBoolopEqual = new RegExp(/==/);
+                //Boolop Not Equal
+                var regBoolopNotEqual = new RegExp(/!=/);
+                //Comment Start
+                var regCommentStart = new RegExp(/\/\*/);
+                //Comment End
+                var regCommentEnd = new RegExp(/\\*\//);
+                //IntOp
+                var regIntOp = new RegExp(/\+/);
+                //Whitespace
+                var regWhitespace = new RegExp(/$|\t$|\n$\r$/);
+                //Newline
+                var regNewline = new RegExp(/\n/);
+                //Keywords
+                //While
+                var regWhile = new RegExp(/while/);
+                //If
+                var regIf = new RegExp(/if/);
+                //Print
+                var regPrint = new RegExp(/print/);
+                //Int
+                var regInt = new RegExp(/int/);
+                //Boolean
+                var regBool = new RegExp(/boolean/);
+                //String
+                var regStr = new RegExp(/string/);
+                //True
+                var regTrue = new RegExp(/true/);
+                //False
+                var regFalse = new RegExp(/false/);
+                // TODO: CHANGE THIS RETURN STATEMENT
                 return sourceCode;
             }
         };
         return Lexer;
-    })();
+    }());
     TSC.Lexer = Lexer;
 })(TSC || (TSC = {}));
--- source/scripts/lexer.ts
+++ source/scripts/lexer.ts
@@ -10,8 +10,77 @@ module TSC
 		        // Trim the leading and trailing spaces.
 		        sourceCode = TSC.Utils.trim(sourceCode);
 		        // TODO: remove all spaces in the middle; remove line breaks too.
-		        return sourceCode;
-		    }
+
+
+				/* Regular Expressions
+				* 	Contains the RegEx for our grammar */
+
+				//Types
+
+				//Character
+				let regChar = new RegExp(/[a-z] | /);
+				//ID
+				let regID = new RegExp(/[a-z]/);
+				//Digit
+				let regDigit = new RegExp(/[0-9]/);
+
+
+
+				//Symbols
+
+				//EOP
+				let regEOP = new RegExp(/\$/);
+				//Left Brace
+				let regLeftBrace = new RegExp(/{/);
+				//Right Brace
+				let regRightBrace = new RegExp(/}/);
+				//Left Parenthesis
+				let regLeftParen = new RegExp(/\(/);
+				//Right Parenthesis
+				let regRightParen = new RegExp(/\)/);
+				//Quote
+				let regQuote = new RegExp(/"/);
+				//Assign
+				let regAssign = new RegExp(/=/);
+				//Boolop Equal
+				let regBoolopEqual = new RegExp(/==/);
+				//Boolop Not Equal
+				let regBoolopNotEqual = new RegExp(/!=/);
+				//Comment Start
+				let regCommentStart = new RegExp(/\/\*/);
+				//Comment End
+				let regCommentEnd = new RegExp(/\\*\//);
+				//IntOp
+				let regIntOp = new RegExp(/\+/);
+				//Whitespace
+				let regWhitespace = new RegExp(/$|\t$|\n$\r$/);
+				//Newline
+				let regNewline = new RegExp(/\n/);
+
+				//Keywords
+
+				//While
+				let regWhile = new RegExp(/while/);
+				//If
+				let regIf = new RegExp(/if/);
+				//Print
+				let regPrint = new RegExp(/print/);
+				//Int
+				let regInt = new RegExp(/int/);
+				//Boolean
+				let regBool = new RegExp(/boolean/);
+				//String
+				let regStr = new RegExp(/string/);
+				//True
+				let regTrue = new RegExp(/true/);
+				//False
+				let regFalse = new RegExp(/false/);
+
+
+				// TODO: CHANGE THIS RETURN STATEMENT
+				return sourceCode;
+			}
 		}
 	}
 	}
+
