d3d9bcb72abe7bd18707f357d907c1f70880863c
alliyahtaylor <serenatskino6@gmail.com>
2019-02-12 00:02:00 -0500

    Fixed regex, added variables
    Added things that I'll need for the actual lexer logic. Fixed all the regular expressions, I think. Made some minor changes to the HTML file. I promise it'll be pretty before project 2.
    ____________________________________
    The AMBASSADOR continues to scurry after Sisko, distractedly rambling off his list of concerns.
    
    Sisko turns suddenly to the AMBASSADOR with a diplomatic smile. The AMBASSADOR stumbles briefly but stops short in front of Sisko.
    
    SISKO (Slowly)
    
    /Ambassador/, I promise you I will do all I can to assure you that you fulfill your mission. In fact -
    
    AMBASSADOR (eagerly)
    Yes?
    
    SISKO
    I'll get back to you at 0900 hours.
    
    Sisko steps back into a TURBOLIFT and the door slides shut, leaving the AMBASSADOR staring at blank metal.

--- dist/scripts/lexer.js
+++ dist/scripts/lexer.js
@@ -5,72 +5,124 @@ var TSC;
         function Lexer() {
         }
         Lexer.lex = function () {
-            {
-                // Grab the "raw" source code.
-                var sourceCode = document.getElementById("taSourceCode").value;
-                // Trim the leading and trailing spaces.
-                sourceCode = TSC.Utils.trim(sourceCode);
-                // TODO: remove all spaces in the middle; remove line breaks too.
-                /* Regular Expressions
-                * 	Contains the RegEx for our grammar */
-                //Types
-                //Character
-                var regChar = new RegExp(/[a-z] | /);
-                //ID
-                var regID = new RegExp(/[a-z]/);
-                //Digit
-                var regDigit = new RegExp(/[0-9]/);
-                //Symbols
-                //EOP
-                var regEOP = new RegExp(/\$/);
-                //Left Brace
-                var regLeftBrace = new RegExp(/{/);
-                //Right Brace
-                var regRightBrace = new RegExp(/}/);
-                //Left Parenthesis
-                var regLeftParen = new RegExp(/\(/);
-                //Right Parenthesis
-                var regRightParen = new RegExp(/\)/);
-                //Quote
-                var regQuote = new RegExp(/"/);
-                //Assign
-                var regAssign = new RegExp(/=/);
-                //Boolop Equal
-                var regBoolopEqual = new RegExp(/==/);
-                //Boolop Not Equal
-                var regBoolopNotEqual = new RegExp(/!=/);
-                //Comment Start
-                var regCommentStart = new RegExp(/\/\*/);
-                //Comment End
-                var regCommentEnd = new RegExp(/\\*\//);
-                //IntOp
-                var regIntOp = new RegExp(/\+/);
-                //Whitespace
-                var regWhitespace = new RegExp(/$|\t$|\n$\r$/);
-                //Newline
-                var regNewline = new RegExp(/\n/);
-                //Keywords
-                //While
-                var regWhile = new RegExp(/while/);
-                //If
-                var regIf = new RegExp(/if/);
-                //Print
-                var regPrint = new RegExp(/print/);
-                //Int
-                var regInt = new RegExp(/int/);
-                //Boolean
-                var regBool = new RegExp(/boolean/);
-                //String
-                var regStr = new RegExp(/string/);
-                //True
-                var regTrue = new RegExp(/true/);
-                //False
-                var regFalse = new RegExp(/false/);
-                // TODO: CHANGE THIS RETURN STATEMENT
-                return sourceCode;
-            }
+            // Grab the "raw" source code.
+            var sourceCode = document.getElementById("taSourceCode").value;
+            // Trim the leading and trailing spaces.
+            sourceCode = TSC.Utils.trim(sourceCode);
+            // TODO: remove all spaces in the middle; remove line breaks too.
+            /*Stuff We'll Need
+             *	Arrays and Variables */
+            //Token Array
+            var tokens = [];
+            //Error Array
+            var errors = [];
+            //Warning Array
+            var warnings = [];
+            //Current line number
+            var line = 1;
+            //Current position in (on???) current line
+            var position = 1;
+            //Pointer for start of current thing being analyzed
+            var startPoint = 0;
+            //Pointer for end of ^
+            var endPoint = 1;
+            //Lets us know if we're at the end of the program
+            var atEOP = false;
+            //status for lexing - Lets us know if we're inside a comment, quote, or in a normal state
+            var status = "normal";
+            //mode for logging
+            var mode = "normal";
+            /* Regular Expressions
+            *	Contains the RegEx for our grammar */
+            //Types
+            //Character
+            var regChar = new RegExp('[a-z] | ');
+            //ID
+            var regID = new RegExp("[a-z]");
+            //Digit
+            var regDigit = new RegExp('[0-9]');
+            //Symbols
+            //EOP
+            var regEOP = new RegExp('\$');
+            //Left Brace
+            var regLeftBrace = new RegExp('{');
+            //Right Brace
+            var regRightBrace = new RegExp('}');
+            //Left Parenthesis
+            var regLeftParen = new RegExp('\\(');
+            //Right Parenthesis
+            var regRightParen = new RegExp('\\)');
+            //Quote
+            var regQuote = new RegExp('"');
+            //Assign
+            var regAssign = new RegExp('=');
+            //Boolop Equal
+            var regBoolopEqual = new RegExp('==');
+            //Boolop Not Equal
+            var regBoolopNotEqual = new RegExp('!=');
+            //Comment Start
+            var regCommentStart = new RegExp('\/\\*');
+            //Comment End
+            var regCommentEnd = new RegExp('\\*\/');
+            //IntOp
+            var regIntOp = new RegExp('\\+');
+            //Whitespace
+            var regWhitespace = new RegExp('$|\t$|\n$\r$');
+            //Newline
+            var regNewline = new RegExp('\n');
+            //Keywords
+            //While
+            var regWhile = new RegExp('while');
+            //If
+            var regIf = new RegExp('if');
+            //Print
+            var regPrint = new RegExp('print');
+            //Int
+            var regInt = new RegExp('int');
+            //Boolean
+            var regBool = new RegExp('boolean');
+            //String
+            var regStr = new RegExp('string');
+            //True
+            var regTrue = new RegExp('true');
+            //False
+            var regFalse = new RegExp('false');
+            // TODO: CHANGE THIS RETURN STATEMENT
+            //leaving for now so intellij doesn't yell at me. I don't like that.
+            return sourceCode;
         };
         return Lexer;
     }());
     TSC.Lexer = Lexer;
+    //Defining a Token object. Haven't decided if this will stay here.
+    var Token = /** @class */ (function () {
+        function Token(name, value, lineNumber, position) {
+            this.name = name;
+            this.value = value;
+            this.lineNumber = lineNumber;
+            this.position = position;
+        }
+        return Token;
+    }());
+    TSC.Token = Token;
+    var Error = /** @class */ (function () {
+        function Error(name, value, lineNumber, position) {
+            this.name = name;
+            this.value = value;
+            this.lineNumber = lineNumber;
+            this.position = position;
+        }
+        return Error;
+    }());
+    TSC.Error = Error;
+    var Warning = /** @class */ (function () {
+        function Warning(name, value, lineNumber, position) {
+            this.name = name;
+            this.value = value;
+            this.lineNumber = lineNumber;
+            this.position = position;
+        }
+        return Warning;
+    }());
+    TSC.Warning = Warning;
 })(TSC || (TSC = {}));
--- source/scripts/lexer.ts
+++ source/scripts/lexer.ts
@@ -4,83 +4,152 @@ module TSC
 	{
 	export class Lexer {
 		public static lex() {
-		    {
-		        // Grab the "raw" source code.
-		        var sourceCode = (<HTMLInputElement>document.getElementById("taSourceCode")).value;
-		        // Trim the leading and trailing spaces.
-		        sourceCode = TSC.Utils.trim(sourceCode);
-		        // TODO: remove all spaces in the middle; remove line breaks too.
 
 
-				/* Regular Expressions
-				* 	Contains the RegEx for our grammar */
+			// Grab the "raw" source code.
+			var sourceCode = (<HTMLInputElement>document.getElementById("taSourceCode")).value;
+			// Trim the leading and trailing spaces.
+			sourceCode = TSC.Utils.trim(sourceCode);
+			// TODO: remove all spaces in the middle; remove line breaks too.
 
-				//Types
+			/*Stuff We'll Need
+             *	Arrays and Variables */
 
-				//Character
-				let regChar = new RegExp(/[a-z] | /);
-				//ID
-				let regID = new RegExp(/[a-z]/);
-				//Digit
-				let regDigit = new RegExp(/[0-9]/);
+			//Token Array
+			let tokens = [];
+			//Error Array
+			let errors = [];
+			//Warning Array
+			let warnings = [];
 
-
-
-				//Symbols
-
-				//EOP
-				let regEOP = new RegExp(/\$/);
-				//Left Brace
-				let regLeftBrace = new RegExp(/{/);
-				//Right Brace
-				let regRightBrace = new RegExp(/}/);
-				//Left Parenthesis
-				let regLeftParen = new RegExp(/\(/);
-				//Right Parenthesis
-				let regRightParen = new RegExp(/\)/);
-				//Quote
-				let regQuote = new RegExp(/"/);
-				//Assign
-				let regAssign = new RegExp(/=/);
-				//Boolop Equal
-				let regBoolopEqual = new RegExp(/==/);
-				//Boolop Not Equal
-				let regBoolopNotEqual = new RegExp(/!=/);
-				//Comment Start
-				let regCommentStart = new RegExp(/\/\*/);
-				//Comment End
-				let regCommentEnd = new RegExp(/\\*\//);
-				//IntOp
-				let regIntOp = new RegExp(/\+/);
-				//Whitespace
-				let regWhitespace = new RegExp(/$|\t$|\n$\r$/);
-				//Newline
-				let regNewline = new RegExp(/\n/);
-
-				//Keywords
-
-				//While
-				let regWhile = new RegExp(/while/);
-				//If
-				let regIf = new RegExp(/if/);
-				//Print
-				let regPrint = new RegExp(/print/);
-				//Int
-				let regInt = new RegExp(/int/);
-				//Boolean
-				let regBool = new RegExp(/boolean/);
-				//String
-				let regStr = new RegExp(/string/);
-				//True
-				let regTrue = new RegExp(/true/);
-				//False
-				let regFalse = new RegExp(/false/);
+			//Current line number
+			let line = 1;
+			//Current position in (on???) current line
+			let position = 1;
+			//Pointer for start of current thing being analyzed
+			let startPoint = 0;
+			//Pointer for end of ^
+			let endPoint = 1;
 
 
+			//Lets us know if we're at the end of the program
+			let atEOP: boolean = false;
+			//status for lexing - Lets us know if we're inside a comment, quote, or in a normal state
+			let status = "normal";
+			//mode for logging
+			let mode = "normal";
+
+			/* Regular Expressions
+            *	Contains the RegEx for our grammar */
+
+			//Types
+
+			//Character
+			let regChar = new RegExp('[a-z] | ');
+			//ID
+			let regID = new RegExp(`[a-z]`);
+			//Digit
+			let regDigit = new RegExp('[0-9]');
+
+
+			//Symbols
+
+			//EOP
+			let regEOP = new RegExp('\$');
+			//Left Brace
+			let regLeftBrace = new RegExp('{');
+			//Right Brace
+			let regRightBrace = new RegExp('}');
+			//Left Parenthesis
+			let regLeftParen = new RegExp('\\(');
+			//Right Parenthesis
+			let regRightParen = new RegExp('\\)');
+			//Quote
+			let regQuote = new RegExp('"');
+			//Assign
+			let regAssign = new RegExp('=');
+			//Boolop Equal
+			let regBoolopEqual = new RegExp('==');
+			//Boolop Not Equal
+			let regBoolopNotEqual = new RegExp('!=');
+			//Comment Start
+			let regCommentStart = new RegExp('\/\\*');
+			//Comment End
+			let regCommentEnd = new RegExp('\\*\/');
+			//IntOp
+			let regIntOp = new RegExp('\\+');
+			//Whitespace
+			let regWhitespace = new RegExp('$|\t$|\n$\r$');
+			//Newline
+			let regNewline = new RegExp('\n');
+
+			//Keywords
+
+			//While
+			let regWhile = new RegExp('while');
+			//If
+			let regIf = new RegExp('if');
+			//Print
+			let regPrint = new RegExp('print');
+			//Int
+			let regInt = new RegExp('int');
+			//Boolean
+			let regBool = new RegExp('boolean');
+			//String
+			let regStr = new RegExp('string');
+			//True
+			let regTrue = new RegExp('true');
+			//False
+			let regFalse = new RegExp('false');
 				// TODO: CHANGE THIS RETURN STATEMENT
-				return sourceCode;
+				//leaving for now so intellij doesn't yell at me. I don't like that.
+			return sourceCode;
+			}
+		}
+
+		//Defining a Token object. Haven't decided if this will stay here.
+		export class Token{
+			name: String;
+			value: String;
+			lineNumber: number;
+			position: number;
+
+			constructor(name: String, value: String, lineNumber: number, position: number){
+				this.name = name;
+				this.value = value;
+				this.lineNumber = lineNumber;
+				this.position = position;
+			}
+		}
+
+
+		export class Error{
+			name: String;
+			value: String;
+			lineNumber: number;
+			position: number;
+
+			constructor(name: String, value: String, lineNumber: number, position: number){
+				this.name = name;
+				this.value = value;
+				this.lineNumber = lineNumber;
+				this.position = position;
+			}
+		}
+
+		export class Warning{
+			name: String;
+			value: String;
+			lineNumber: number;
+			position: number;
+
+			constructor(name: String, value: String, lineNumber: number, position: number){
+				this.name = name;
+				this.value = value;
+				this.lineNumber = lineNumber;
+				this.position = position;
 			}
 		}
 	}
-	}
+
 
