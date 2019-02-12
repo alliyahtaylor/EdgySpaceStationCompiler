074242fbe3ab1eb66cf2e5f5e7377a09cf8aa00d
alliyahtaylor <serenatskino6@gmail.com>
2019-02-12 15:17:28 -0500

    Playing around with regex, adding some rudimentary logic
    and by rudimentary logic I mean I copied and pasted what worked over and over again and now I have to fix it.
    ____________________________________
    INT. ANOTHER ANGLE
    
    The AMBASSADOR huffs and pulls a TOUPEE off of his head, revealing an incredibly receeding hairline. He looks at another person across the room with the same balding pattern and shakes his head.
    
    The AMBASSADOR, who we come to know as VIR COTTO, scurries across the room to the man who will will come to know as LONDO MOLLARI.
    
    LONDO (expectantly)
    Well?
    
    VIR
    (sighs and removes his fake pointed ears) Nothing. These "Starfleet" people are more stubborn than Earth Force.
    
    LONDO
    Bah! You simply did not ask the right questions. Return to my quarters and I will attempt to interrogate this "Quark" fellow at the bar.
    
    VIR
    But Sir-
    
    LONDO
    Quarters, Vir, I don't have time for this.

--- dist/scripts/lexer.js
+++ dist/scripts/lexer.js
@@ -20,15 +20,15 @@ var TSC;
             var warnings = [];
             //Current line number
             var line = 1;
-            //Current position in (on???) current line
+            //Current position in current line
             var position = 1;
             //Pointer for start of current thing being analyzed
             var startPoint = 0;
-            //Pointer for end of ^
+            //Pointer for end
             var endPoint = 1;
             //Lets us know if we're at the end of the program
             var atEOP = false;
-            //status for lexing - Lets us know if we're inside a comment, quote, or in a normal state
+            //status for if we're in a quote, comment, or normal program
             var status = "normal";
             //mode for logging
             var mode = "normal";
@@ -49,9 +49,9 @@ var TSC;
             //Right Brace
             var regRightBrace = new RegExp('}');
             //Left Parenthesis
-            var regLeftParen = new RegExp('\\(');
+            var regLeftParen = new RegExp('\\x28');
             //Right Parenthesis
-            var regRightParen = new RegExp('\\)');
+            var regRightParen = new RegExp('\\x29');
             //Quote
             var regQuote = new RegExp('"');
             //Assign
@@ -67,7 +67,7 @@ var TSC;
             //IntOp
             var regIntOp = new RegExp('\\+');
             //Whitespace
-            var regWhitespace = new RegExp('$|\t$|\n$\r$');
+            var regWhitespace = new RegExp(' |\t|\n\r');
             //Newline
             var regNewline = new RegExp('\n');
             //Keywords
@@ -87,6 +87,183 @@ var TSC;
             var regTrue = new RegExp('true');
             //False
             var regFalse = new RegExp('false');
+            console.log("test");
+            //Iterating through the source code
+            while (endPoint <= sourceCode.length) {
+                var i = 0;
+                console.log("where" + i);
+                i++;
+                atEOP = false;
+                //TODO figure out if I can reduce this regex stuff because eww
+                switch (status) {
+                    case "comment": {
+                        if (regNewline.test(sourceCode.substring(startPoint, endPoint))) {
+                            //I don't actually know how verbose works but I'm gonna check
+                            if (mode = "verbose") {
+                                console.log("NEWLINE");
+                            }
+                            line++;
+                            position = 0;
+                        }
+                        if (regCommentEnd.test(sourceCode.substring(startPoint, endPoint))) {
+                            status = "normal";
+                        }
+                        endPoint++;
+                        console.log("comment");
+                        continue;
+                    }
+                    case "string": {
+                        //Check for Character
+                        if (regChar.test(sourceCode.charAt(endPoint - 1))) {
+                            var token = new Token('TChar', sourceCode.charAt(endPoint - 1), line, position);
+                            tokens.push(token);
+                            position++;
+                            //Check for an end quote
+                        }
+                        else if (regQuote.test(sourceCode.charAt(endPoint - 1))) {
+                            var token = new Token('TQuote', sourceCode.charAt(endPoint - 1), line, position);
+                            tokens.push(token);
+                            position++;
+                            status = "normal";
+                            //TODO: Make enum for mode and status, also better var names
+                            mode = "normal";
+                            //Throw an error if we hit something that's not a character or a quote
+                        }
+                        else {
+                            console.log("Error: Invalid token in String.");
+                            var char = sourceCode.charAt(endPoint - 1);
+                            var error = new Error("InvalidToken", char, line, position);
+                            errors.push(error);
+                            break;
+                        }
+                        endPoint++;
+                        console.log("string");
+                        continue;
+                    }
+                    case "normal": {
+                        //Figure out what order to look for this stuff
+                        //Left Brace - {
+                        if (regLeftBrace.test(sourceCode.substring(startPoint, endPoint))) {
+                            var token = new Token("TLeftBrace", sourceCode.charAt(endPoint - 1), line, position);
+                            tokens.push(token);
+                        }
+                        //Right Brace - }
+                        else if (regRightBrace.test(sourceCode.substring(startPoint, endPoint))) {
+                            var token_1 = new Token("TRightBrace", sourceCode.charAt(endPoint - 1), line, position);
+                            tokens.push(token_1);
+                        }
+                        //Left Paren - (
+                        else if (regLeftParen.test(sourceCode.substring(startPoint, endPoint))) {
+                            var token = new Token("TLeftParen", sourceCode.charAt(endPoint - 1), line, position);
+                            tokens.push(token);
+                        }
+                        //Right Paren - )
+                        else if (regRightParen.test(sourceCode.substring(startPoint, endPoint))) {
+                            var token = new Token("TRightParen", sourceCode.charAt(endPoint - 1), line, position);
+                            tokens.push(token);
+                        }
+                        //Quote
+                        else if (regQuote.test(sourceCode.substring(startPoint, endPoint))) {
+                            var token = new Token("TQuote", sourceCode.charAt(endPoint - 1), line, position);
+                            status = "string";
+                            tokens.push(token);
+                        }
+                        /*  Keyword time! */
+                        //TODO Deal with adding tokens that aren't single chars to token item
+                        //While
+                        else if (regWhile.test(sourceCode.substring(startPoint, endPoint))) {
+                            var token = new Token("TWhile", sourceCode.charAt(endPoint - 1), line, position);
+                            tokens.push(token);
+                        }
+                        //If
+                        else if (regIf.test(sourceCode.substring(startPoint, endPoint))) {
+                            var token = new Token("TIf", sourceCode.charAt(endPoint - 1), line, position);
+                            tokens.push(token);
+                        }
+                        //Boolean True
+                        else if (regTrue.test(sourceCode.substring(startPoint, endPoint))) {
+                            var token = new Token("TTrue", sourceCode.charAt(endPoint - 1), line, position);
+                            tokens.push(token);
+                        }
+                        //Boolean False
+                        else if (regFalse.test(sourceCode.substring(startPoint, endPoint))) {
+                            var token = new Token("TFalse", sourceCode.charAt(endPoint - 1), line, position);
+                            tokens.push(token);
+                        }
+                        //Print
+                        else if (regIf.test(sourceCode.substring(startPoint, endPoint))) {
+                            var token = new Token("TPrint", sourceCode.charAt(endPoint - 1), line, position);
+                            tokens.push(token);
+                        }
+                        //"Int"
+                        else if (regInt.test(sourceCode.substring(startPoint, endPoint))) {
+                            var token = new Token("TInt", sourceCode.charAt(endPoint - 1), line, position);
+                            tokens.push(token);
+                        }
+                        //"String"
+                        else if (regStr.test(sourceCode.substring(startPoint, endPoint))) {
+                            var token = new Token("TString", sourceCode.charAt(endPoint - 1), line, position);
+                            tokens.push(token);
+                        }
+                        //"Boolean"
+                        else if (regBool.test(sourceCode.substring(startPoint, endPoint))) {
+                            var token = new Token("TBoolean", sourceCode.charAt(endPoint - 1), line, position);
+                            tokens.push(token);
+                        }
+                        //"Boolean"
+                        else if (regBool.test(sourceCode.substring(startPoint, endPoint))) {
+                            var token = new Token("TBoolean", sourceCode.charAt(endPoint - 1), line, position);
+                            tokens.push(token);
+                        }
+                        /* End of Keywords */
+                        //Assign
+                        else if (regAssign.test(sourceCode.substring(startPoint, endPoint))) {
+                            var token = new Token("TAssign", sourceCode.charAt(endPoint - 1), line, position);
+                            tokens.push(token);
+                        }
+                        //Boolean Equals - This maybe should go first with the same logic as the keyword placement? will run tests.
+                        else if (regBoolopEqual.test(sourceCode.substring(startPoint, endPoint))) {
+                            var token = new Token("TBooleanEquals", sourceCode.charAt(endPoint - 1), line, position);
+                            tokens.push(token);
+                        }
+                        //intop
+                        else if (regIntOp.test(sourceCode.substring(startPoint, endPoint))) {
+                            var token = new Token("TIntOp", sourceCode.charAt(endPoint - 1), line, position);
+                            tokens.push(token);
+                        }
+                        //digit
+                        else if (regDigit.test(sourceCode.substring(startPoint, endPoint))) {
+                            var token = new Token("TDigit", sourceCode.charAt(endPoint - 1), line, position);
+                            tokens.push(token);
+                        }
+                        //ID
+                        else if (regID.test(sourceCode.substring(startPoint, endPoint))) {
+                            var token = new Token("TID", sourceCode.charAt(endPoint - 1), line, position);
+                            tokens.push(token);
+                        }
+                        //whitespace
+                        else if (regWhitespace.test(sourceCode.substring(startPoint, endPoint))) {
+                            var token = new Token("TWhiteSpace", sourceCode.charAt(endPoint - 1), line, position);
+                            tokens.push(token);
+                        }
+                        //eop
+                        else if (regEOP.test(sourceCode.substring(startPoint, endPoint))) {
+                            var token = new Token("TBooleanEquals", sourceCode.charAt(endPoint - 1), line, position);
+                            tokens.push(token);
+                        }
+                        //errors
+                        else {
+                            //this is just testing, please ignore
+                            console.log("error");
+                        }
+                        endPoint++;
+                        position++;
+                        console.log("normal");
+                        console.log(tokens);
+                        continue;
+                    }
+                }
+            }
             // TODO: CHANGE THIS RETURN STATEMENT
             //leaving for now so intellij doesn't yell at me. I don't like that.
             return sourceCode;
--- source/scripts/lexer.ts
+++ source/scripts/lexer.ts
@@ -24,17 +24,16 @@ module TSC
 
 			//Current line number
 			let line = 1;
-			//Current position in (on???) current line
+			//Current position in current line
 			let position = 1;
 			//Pointer for start of current thing being analyzed
 			let startPoint = 0;
-			//Pointer for end of ^
+			//Pointer for end
 			let endPoint = 1;
-
 
 			//Lets us know if we're at the end of the program
 			let atEOP: boolean = false;
-			//status for lexing - Lets us know if we're inside a comment, quote, or in a normal state
+			//status for if we're in a quote, comment, or normal program
 			let status = "normal";
 			//mode for logging
 			let mode = "normal";
@@ -61,9 +60,9 @@ module TSC
 			//Right Brace
 			let regRightBrace = new RegExp('}');
 			//Left Parenthesis
-			let regLeftParen = new RegExp('\\(');
+			let regLeftParen = new RegExp('\\x28');
 			//Right Parenthesis
-			let regRightParen = new RegExp('\\)');
+			let regRightParen = new RegExp('\\x29');
 			//Quote
 			let regQuote = new RegExp('"');
 			//Assign
@@ -79,7 +78,7 @@ module TSC
 			//IntOp
 			let regIntOp = new RegExp('\\+');
 			//Whitespace
-			let regWhitespace = new RegExp('$|\t$|\n$\r$');
+			let regWhitespace = new RegExp(' |\t|\n\r');
 			//Newline
 			let regNewline = new RegExp('\n');
 
@@ -101,6 +100,186 @@ module TSC
 			let regTrue = new RegExp('true');
 			//False
 			let regFalse = new RegExp('false');
+			console.log("test");
+
+			//Iterating through the source code
+			while (endPoint <= sourceCode.length) {
+				var i = 0;
+				console.log("where" + i);
+				i++;
+				atEOP = false;
+
+				//TODO figure out if I can reduce this regex stuff because eww
+				switch (status) {
+					case "comment": {
+						if (regNewline.test(sourceCode.substring(startPoint, endPoint))) {
+							//I don't actually know how verbose works but I'm gonna check
+							if (mode = "verbose") {
+								console.log("NEWLINE");
+							}
+							line++;
+							position = 0;
+						}
+						if (regCommentEnd.test(sourceCode.substring(startPoint, endPoint))) {
+							status = "normal";
+						}
+						endPoint++;
+						console.log("comment");
+						continue;
+					}
+					case "string": {
+						//Check for Character
+						if (regChar.test(sourceCode.charAt(endPoint - 1))) {
+							var token: Token = new Token('TChar', sourceCode.charAt(endPoint - 1), line, position);
+							tokens.push(token);
+							position++;
+							//Check for an end quote
+						} else if (regQuote.test(sourceCode.charAt(endPoint - 1))) {
+							var token: Token = new Token('TQuote', sourceCode.charAt(endPoint - 1), line, position);
+							tokens.push(token);
+							position++;
+							status = "normal"
+							//TODO: Make enum for mode and status, also better var names
+							mode = "normal";
+							//Throw an error if we hit something that's not a character or a quote
+						} else {
+							console.log("Error: Invalid token in String.");
+							let char = sourceCode.charAt(endPoint - 1);
+							var error: Error = new Error("InvalidToken", char, line, position);
+							errors.push(error);
+							break;
+						}
+						endPoint++;
+						console.log("string");
+						continue;
+					}
+					case "normal": {
+						//Figure out what order to look for this stuff
+						//Left Brace - {
+						if (regLeftBrace.test(sourceCode.substring(startPoint, endPoint))) {
+							var token: Token = new Token("TLeftBrace", sourceCode.charAt(endPoint - 1), line, position);
+							tokens.push(token);
+						}
+						//Right Brace - }
+						else if (regRightBrace.test(sourceCode.substring(startPoint, endPoint))) {
+							let token: Token = new Token("TRightBrace", sourceCode.charAt(endPoint - 1), line, position);
+							tokens.push(token);
+						}
+						//Left Paren - (
+						else if (regLeftParen.test(sourceCode.substring(startPoint, endPoint))) {
+							var token: Token = new Token("TLeftParen", sourceCode.charAt(endPoint - 1), line, position);
+							tokens.push(token);
+						}
+						//Right Paren - )
+						else if (regRightParen.test(sourceCode.substring(startPoint, endPoint))) {
+							var token: Token = new Token("TRightParen", sourceCode.charAt(endPoint - 1), line, position);
+							tokens.push(token);
+						}
+						//Quote
+						else if (regQuote.test(sourceCode.substring(startPoint, endPoint))) {
+							var token: Token = new Token("TQuote", sourceCode.charAt(endPoint - 1), line, position);
+							status = "string";
+							tokens.push(token);
+						}
+						/*  Keyword time! */
+						//TODO Deal with adding tokens that aren't single chars to token item
+						//While
+						else if (regWhile.test(sourceCode.substring(startPoint, endPoint))) {
+							var token: Token = new Token("TWhile", sourceCode.charAt(endPoint - 1), line, position);
+							tokens.push(token);
+						}
+						//If
+						else if (regIf.test(sourceCode.substring(startPoint, endPoint))) {
+							var token: Token = new Token("TIf", sourceCode.charAt(endPoint - 1), line, position);
+							tokens.push(token);
+						}
+						//Boolean True
+						else if (regTrue.test(sourceCode.substring(startPoint, endPoint))) {
+							var token: Token = new Token("TTrue", sourceCode.charAt(endPoint - 1), line, position);
+							tokens.push(token);
+						}
+						//Boolean False
+						else if (regFalse.test(sourceCode.substring(startPoint, endPoint))) {
+							var token: Token = new Token("TFalse", sourceCode.charAt(endPoint - 1), line, position);
+							tokens.push(token);
+						}
+						//Print
+						else if (regIf.test(sourceCode.substring(startPoint, endPoint))) {
+							var token: Token = new Token("TPrint", sourceCode.charAt(endPoint - 1), line, position);
+							tokens.push(token);
+						}
+						//"Int"
+						else if (regInt.test(sourceCode.substring(startPoint, endPoint))) {
+							var token: Token = new Token("TInt", sourceCode.charAt(endPoint - 1), line, position);
+							tokens.push(token);
+						}
+						//"String"
+						else if (regStr.test(sourceCode.substring(startPoint, endPoint))) {
+							var token: Token = new Token("TString", sourceCode.charAt(endPoint - 1), line, position);
+							tokens.push(token);
+						}
+						//"Boolean"
+						else if (regBool.test(sourceCode.substring(startPoint, endPoint))) {
+							var token: Token = new Token("TBoolean", sourceCode.charAt(endPoint - 1), line, position);
+							tokens.push(token);
+						}
+						//"Boolean"
+						else if (regBool.test(sourceCode.substring(startPoint, endPoint))) {
+							var token: Token = new Token("TBoolean", sourceCode.charAt(endPoint - 1), line, position);
+							tokens.push(token);
+						}
+
+						/* End of Keywords */
+						//Assign
+						else if (regAssign.test(sourceCode.substring(startPoint, endPoint))) {
+							var token: Token = new Token("TAssign", sourceCode.charAt(endPoint - 1), line, position);
+							tokens.push(token);
+						}
+						//Boolean Equals - This maybe should go first with the same logic as the keyword placement? will run tests.
+						else if (regBoolopEqual.test(sourceCode.substring(startPoint, endPoint))) {
+							var token: Token = new Token("TBooleanEquals", sourceCode.charAt(endPoint - 1), line, position);
+							tokens.push(token);
+						}
+						//intop
+						else if (regIntOp.test(sourceCode.substring(startPoint, endPoint))) {
+							var token: Token = new Token("TIntOp", sourceCode.charAt(endPoint - 1), line, position);
+							tokens.push(token);
+						}
+						//digit
+						else if (regDigit.test(sourceCode.substring(startPoint, endPoint))) {
+							var token: Token = new Token("TDigit", sourceCode.charAt(endPoint - 1), line, position);
+							tokens.push(token);
+						}
+						//ID
+						else if (regID.test(sourceCode.substring(startPoint, endPoint))) {
+							var token: Token = new Token("TID", sourceCode.charAt(endPoint - 1), line, position);
+							tokens.push(token);
+						}
+						//whitespace
+						else if (regWhitespace.test(sourceCode.substring(startPoint, endPoint))) {
+							var token: Token = new Token("TWhiteSpace", sourceCode.charAt(endPoint - 1), line, position);
+							tokens.push(token);
+						}
+						//eop
+						else if (regEOP.test(sourceCode.substring(startPoint, endPoint))) {
+							var token: Token = new Token("TBooleanEquals", sourceCode.charAt(endPoint - 1), line, position);
+							tokens.push(token);
+						}
+						//errors
+						else {
+							//this is just testing, please ignore
+							console.log("error");
+						}
+						endPoint++;
+						position++;
+						console.log("normal");
+						console.log(tokens);
+						continue;
+					}
+
+				}
+
+			}
 				// TODO: CHANGE THIS RETURN STATEMENT
 				//leaving for now so intellij doesn't yell at me. I don't like that.
 			return sourceCode;
