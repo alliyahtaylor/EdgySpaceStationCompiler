9d1ccbd523a4e38c34268ef1affa00c2f83326eb
Alliyah Taylor <serenatskino6@gmail.com>
2019-03-11 14:06:04 -0400

    Started parser skeleton
    guess who waited entirely too long to start again? It's me. It's always me. (minus infinity, I know, I know.)
    Doesn't help that everyone kept telling me they finished in a day.
    _________________________________________________________
    Int. Quark's
    
    ODO watches LONDO and QUARK from across the bar. He is holding a drink that he does not drink from and is not in uniform. He glares as QUARK walks LONDO through his first game of DABO, which LONDO appears to be doing quite well at. The DABO GIRL cheers and LONDO collects his winnings before betting on another game. ODO shakes his head and leaves in a hurry.
    
    END OF TEASER

---
+++ source/scripts/parser.ts
@@ -0,0 +1,106 @@
+/*parser.ts*/
+
+module TSC
+{
+    export class Parser {
+
+
+        constructor(){
+        }
+
+        public parse(){
+        }
+
+        public parseProg(){
+
+        }
+
+        public parseBlock(){
+
+        }
+
+        public parseStatementList(){
+
+        }
+
+        public parseStatement(){
+
+        }
+
+        public parsePrint(){
+
+        }
+
+        public parseAssignment(){
+
+        }
+
+        public parseVarDecl(){
+
+        }
+
+        public parseWhile(){
+
+        }
+
+        public parseIf(){
+
+        }
+
+        public parseExpr(){
+
+        }
+
+        public parseInt(){
+
+        }
+
+        public parseString(){
+
+        }
+
+        public parseBoolean(){
+
+        }
+
+        public parseID(){
+
+        }
+
+        public parseCharList(){
+
+        }
+
+        public parseType(){
+
+        }
+
+        public parseChar(){
+
+        }
+
+        public parseSpace(){
+
+        }
+
+        public parseDigit(){
+
+        }
+
+        public parseBoolOp(){
+
+        }
+
+        public parseBoolVal(){
+
+        }
+        public parseIntOp(){
+
+        }
+
+        public match(){
+
+        }
+
+    }
+}
